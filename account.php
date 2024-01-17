7097748c58356ab3860e99b7369f054225a8c129
unknown <ioncotinosu12@gmail.com>
2024-01-17 19:45:12 +0200

    profile version2

--- account.php
+++ account.php
@@ -1,5 +1,16 @@
 <?php
 session_start();
 $user = $_SESSION['auth_user'];
+$conn = new mysqli("localhost", "root", "", "daw-app");
+if ($conn->connect_error) {
+    die("Eroare: " . $conn->connect_error);
+}
+$result = $conn->query(
+    "SELECT * FROM profiles WHERE username = '$user'"
+);
+if ($result->num_rows > 0) {
+    $_SESSION['user_profiles'] = array_column($result->fetch_all(MYSQLI_ASSOC),"profilename");
+}
+
 require "html/account.html"
 ?>
